50d7d39f8cfa10cca29fd9b15af42ee6475830b2
ScreenshotRU <screenshot@inbox.ru>
2020-04-07 20:28:20 +0700

    components TodoItem and TodoList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import TodoList from './Todo/TodoList'
 
 function App() {
     return (<div className='wrapper'>
-        <h1>React tutorial</h1>        
+        <h1>React tutorial</h1>      
+          
+        <TodoList/>
     </div>)
 }
 
